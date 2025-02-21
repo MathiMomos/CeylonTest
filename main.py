83aabc714abae2c57844b2dd0cd7c84483f8326e
Leo <leonidasgarcialescano@gmail.com>
2025-02-21 16:17:24 -0500

    PRIMERA VERSION DEL SWITCH CONTROL

--- main.py
+++ main.py
@@ -5,28 +5,7 @@ from CeylonInterpreter.Interpreter.interpreter import Interpreter
 
 text = \
 '''
-cond = true;
 
-while (cond) {
-    print("=== Menú Principal ===");
-    print("1. Saludar");
-    print("2. Mostrar número favorito");
-    print("3. Salir");
-    print("Selecciona una opción: ");
-
-    scan(opcion);
-
-    if (opcion == "1") {
-        print("¡Hola! ¿Cómo estás?");
-    } elif (opcion == "2") {
-        print("Mi número favorito es el 7.");
-    } elif (opcion == "3") {
-        print("Saliendo del programa...");
-        cond = false;
-    } else {
-        print("Opción no válida. Intenta de nuevo.");
-    }
-}
 '''
 
 parser = Parser()
